74caf3c3452f4c57006f69c8a806d131bce1ab68
zzm <1299076979@qq.com>
2024-06-07 16:50:21 +0800

    remove

--- .history/hardhat.config_20240604163211.js
+++
@@ -1,8 +0,0 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ignition");
-require("dotenv").config();
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24",
-};
